328da47297c8126e8ec9e2fa2811dc96cffd0da4
Atakan Çetinkaya <atakanc1405@gmail.com>
2022-11-22 13:15:51 +0300

    Add files via upload

---
+++ App_Code/VeriTabani.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.OleDb;
+
+namespace _1912901057_AtakanCetinkaya
+{
+    public static class VeriTabani
+    {
+        public static OleDbConnection con;
+
+        public static OleDbConnection Con
+        {
+            get
+            {
+                if(con == null)
+                {
+                   con = new OleDbConnection("Provider = Microsoft.Jet.OLEDB.4.0; Data Source = |DataDirectory|/vt.mdb");
+                }
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+                return con;
+            }
+        }
+
+        public static DataTable veriGetir(string sql)
+        {
+            OleDbConnection con = new OleDbConnection("Provider = Microsoft.Jet.OLEDB.4.0; Data Source = |DataDirectory|/vt.mdb");
+            con.Open();
+            OleDbDataAdapter da = new OleDbDataAdapter(sql, Con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            return dt;
+        }
+
+        public static OleDbCommand KomutOlustur(string sql)
+        {
+            OleDbCommand cmd = new OleDbCommand(sql, Con);
+            return cmd;
+        }
+
+        public static int KomutCalistir(OleDbCommand cmd)
+        {
+            int s = cmd.ExecuteNonQuery();
+            return s;
+        }
+       
+    }
+}
